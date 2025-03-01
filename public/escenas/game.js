aa77d21acdd9f62a029384ee9b73f7fa6bb4fb40
Rodolfo Solaro <rodolfo.solaro@autodesk.com>
2025-03-01 16:09:49 -0300

    Resettear multiplicadorCombustible

--- public/escenas/game.js
+++ public/escenas/game.js
@@ -243,6 +243,7 @@ class EscenaPrincipal extends Phaser.Scene {
         this.entidades[`avion_${avion.numeroAvion}`].torpedo = true;
         this.entidades[`avion_${avion.numeroAvion}`].seleccionado = false;
         this.entidades[`avion_${avion.numeroAvion}`].combustible = this.estadoJuego.entidades[`avion_${avion.numeroAvion}`].combustible;
+        this.entidades[`avion_${avion.numeroAvion}`].multiplicadorCombustible = 1;
         this.entidades[`avion_${avion.numeroAvion}`].indicadorCombustible.setVisible(false);
         avion.setVisible(false); 
         let botonAmodificar = this.botonesAviones[`${avion.numeroAvion}`-1];
