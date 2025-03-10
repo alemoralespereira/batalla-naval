4801d611128c4bf0a5e3782b1b922061f003f863
gregueira <gregueira50@gmail.com>
2025-03-10 15:24:23 -0300

    Update config.js

--- servidor/config.js
+++ servidor/config.js
@@ -6,3 +6,12 @@ export const DB_PORT = process.env.PORTDB
 export const DB_NAME = process.env.DB
 export const DB_USER = process.env.USERDB
 export const DB_PASSWORD = process.env.PASSWORDDB
+
+export default {
+    PUERTO,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD
+};
